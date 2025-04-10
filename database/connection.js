e0833369512a35c4501580e8ffb7eb335b26f78e
testinglife001 <testing.life001@gmail.com>
2025-04-10 14:07:26 +0600

    db issue mongoose connect putting MONGO_URI  fixing

--- database/connection.js
+++ database/connection.js
@@ -4,9 +4,9 @@ import mongoose from "mongoose";
 
 export const connectDB = async()=>{
     try {
-         const conn = await mongoose.connect(process.env.MONGO_URI,
-        //const conn = await mongoose.connect(
-        //    "mongodb+srv://testinglife001:MkDV77J0x1fniWB4@mysimpleapp.ndaxdri.mongodb.net/?retryWrites=true&w=majority&appName=mysimpleapp",
+        // const conn = await mongoose.connect(process.env.MONGO_URI,
+        const conn = await mongoose.connect(
+            "mongodb+srv://testinglife001:MkDV77J0x1fniWB4@mysimpleapp.ndaxdri.mongodb.net/?retryWrites=true&w=majority&appName=mysimpleapp",
             {
             useNewUrlParser:true,
             useCreateIndex: true
