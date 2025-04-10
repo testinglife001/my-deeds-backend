7de9ce7b39561b0d6af6bd735fd51f902933fb99
testinglife001 <testing.life001@gmail.com>
2025-04-10 14:17:24 +0600

    removing useNewUrlParser from mongoose connect

--- database/connection.js
+++ database/connection.js
@@ -6,11 +6,11 @@ export const connectDB = async()=>{
     try {
         // const conn = await mongoose.connect(process.env.MONGO_URI,
         const conn = await mongoose.connect(
-            "mongodb+srv://testinglife001:MkDV77J0x1fniWB4@mysimpleapp.ndaxdri.mongodb.net/?retryWrites=true&w=majority&appName=mysimpleapp",
-            {
-            useNewUrlParser:true,
+            "mongodb+srv://testinglife001:MkDV77J0x1fniWB4@mysimpleapp.ndaxdri.mongodb.net/?retryWrites=true&w=majority&appName=mysimpleapp"
+            // {
+            // useNewUrlParser:true,
             // useCreateIndex: true
-            }
+            //  }
         )
         console.log(`MongoDB Connected: ${conn.connection.host}`);
         
